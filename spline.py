b50f2443bfd3711e5e46c74bd7fa139ae6673015
Randy Heiland <randy.heiland@gmail.com>
2023-08-11 06:13:56 -0400

    update

--- spline.py
+++ spline.py
@@ -18,11 +18,21 @@ class MplWidget(QtWidgets.QWidget):
         vertical_layout = QtWidgets.QVBoxLayout(self)
         vertical_layout.addWidget(self.canvas)
 
+        self.instructions = f"`c` clear; `u` undo; `n` new type; `s` save"
+        self.cell_type_name = "epi"
+        self.cell_volume = 2494.0
+        self.cell_radius = (self.cell_volume * 0.75 / np.pi) ** (1./3)
+        print(f"default self.cell_radius= {self.cell_radius}")
+        # self.cell_radius = 8.412710547954228   # from PhysiCell_phenotype.cpp
+        self.color_by_celltype = ['gray','red','green','yellow','cyan','magenta','blue','brown','black','orange','seagreen','gold']
+
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.ax0 = self.canvas.axes
+        self.ax0.set_title(self.instructions, fontsize=10)
 
         self.num_pts = 0
         self.bezpts = np.zeros((4,2))
+        self.bez_plot = []
         print("self.bezpts=",self.bezpts)
         self.num_eval = 10
 
@@ -30,6 +40,9 @@ class MplWidget(QtWidgets.QWidget):
         self.plot_xmax = 500
         self.plot_ymin = -500
         self.plot_ymax = 500
+
+        self.xv = None  # xvalues of points along Bezier curve
+        self.yv = None  # yvalues of points along Bezier curve
 
         self.ax0.set_aspect(1.0)
         self.ax0.set_xlim(self.plot_xmin, self.plot_xmax)
@@ -40,18 +53,44 @@ class MplWidget(QtWidgets.QWidget):
         # self.canvas.mpl_connect("motion_notify_event", self.on_move)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.test_method()
-        elif event.key() == Qt.Key_C:
+        # if event.key() == Qt.Key_Space:
+            # self.test_method()
+        if event.key() == Qt.Key_C:
             self.ax0.cla()
             self.ax0.set_aspect(1.0)
             self.ax0.set_xlim(self.plot_xmin, self.plot_xmax)
             self.ax0.set_ylim(self.plot_ymin, self.plot_ymax)
+            self.ax0.set_title(self.instructions, fontsize=10)
             self.canvas.update()
             self.canvas.draw()
-
-    def test_method(self):
-        print('Space key pressed')
+        elif event.key() == Qt.Key_U:
+            # print("----- Undo is TBD...")
+            print("# plots= ",len(self.bez_plot))
+            print(type(self.bez_plot))
+            print(self.bez_plot)
+            # self.bez_plot = self.bez_plot.pop()
+            self.bez_plot.pop()
+            # self.bez_plot.del[-1]
+            print("# plots= ",len(self.bez_plot))
+            self.update_plots()
+        elif event.key() == Qt.Key_N:
+            # print("----- New cell type ...")
+            self.cell_type_name = input("Enter cell type name:    (disregard this--> ")
+            print("-- new cell type name is ", self.cell_type_name)
+            volume = input("Enter cell type volume:     (disregard this--> ")
+            self.cell_volume = float(volume)
+            self.cell_radius = (self.cell_volume * 0.75 / np.pi) ** (1./3)
+            print(f"-- its volume is {self.cell_volume} (and r={self.cell_radius}")
+        elif event.key() == Qt.Key_S:
+            fname = "curvy.csv"
+            print(f"----- Writing to {fname}...")
+            with open(fname, 'w') as f:
+                f.write('x,y,z,type,volume,cycle entry,custom:GFP,custom:sample\n')
+                for idx in range(len(self.bez_plot)):
+                    xvals = self.bez_plot[idx][:,0]
+                    yvals = self.bez_plot[idx][:,1]
+                    for jdx in range(len(xvals)):
+                        f.write(f"{xvals[jdx]},{yvals[jdx]},0,{self.cell_type_name}\n")
 
     #---------------------------------------------------------------------------
     def circles(self, x, y, s, c='b', vmin=None, vmax=None, **kwargs):
@@ -131,6 +170,50 @@ class MplWidget(QtWidgets.QWidget):
         # return collection
 
     #-----------------------------------------
+    # def update_plots(self,xvals,yvals):
+    def update_plots(self):
+        print("----------------  update_plots -----------------")
+        self.ax0.cla()
+
+        # print(f"------- update_plots(): type(xvals)={type(xvals)}")
+        # print(f"        type(xvals)={type(xvals)}")
+        # print(f"        xvals.shape={xvals.shape}")
+        # print(f"        self.bezpts={self.bezpts}")
+
+        print(f"        len(self.bez_plot={len(self.bez_plot)}")
+        rval = 12.0
+        for idx in range(len(self.bez_plot)):
+            xvals = self.bez_plot[idx][:,0]
+            yvals = self.bez_plot[idx][:,1]
+            print("xvals=", xvals)
+            print("yvals=", yvals)
+            self.ax0.plot(xvals, yvals)   # plot 4-pt control polygon
+
+            # self.xv, self.yv= self.bezier_curve(self.bezpts,40)  # eval Bezier
+            # self.xv, self.yv= self.bezier_curve(self.bez_plot[idx],40)  # eval Bezier
+            xvals, yvals= self.bezier_curve(self.bez_plot[idx],40)  # eval Bezier
+
+            # self.circles(self.xv,self.yv, s=rval, c='r', edgecolor='red')
+            self.circles(xvals,yvals, s=rval, c='r', edgecolor='red')
+
+            # self.xv2, self.yv2= self.cell_spacing(self.xv,self.yv)  # eval Bezier
+
+            self.ax0.plot(xvals, yvals)   # plot 4-pt control polygon
+
+        # self.ax0.set_xlabel('time (mins)')
+        # self.ax0.set_ylabel('# of cells')
+        # self.ax0.set_title("'c' to clear all.", fontsize=10)
+        self.ax0.set_title(self.instructions, fontsize=10)
+
+        self.ax0.set_aspect(1.0)
+        self.ax0.set_xlim(self.plot_xmin, self.plot_xmax)
+        self.ax0.set_ylim(self.plot_ymin, self.plot_ymax)
+        # self.ax0.legend(loc='center right', prop={'size': 8})
+        self.canvas.update()
+        self.canvas.draw()
+        return
+
+    #-----------------------------------------
     def bernstein_poly(self, i, n, t):
         """
         The Bernstein polynomial of n, i as a function of t
@@ -167,9 +250,16 @@ class MplWidget(QtWidgets.QWidget):
         return xvals, yvals
 
     #---------------------------------------------------------------------------
+    def cell_spacing(self, xv,yv):  # eval Bezier
+        return xv, yv
+
+    #---------------------------------------------------------------------------
     def button_press(self, event):
+        print("----------------  button_press -----------------")
         xval = event.xdata
         yval = event.ydata
+        if xval is None:
+            return
         print("xval,yval=", xval,yval)
         self.bezpts[self.num_pts] = [xval,yval]
         print("self.bezpts=",self.bezpts)
@@ -193,10 +283,16 @@ class MplWidget(QtWidgets.QWidget):
             print("------- plot Bezier!")
             # xv, yv= bezier_curve(bezpts, nTimes=num_eval)  # eval Bezier
             # xv, yv= self.bezier_curve(self.bezpts, nTimes=self.num_eval)  # eval Bezier
-            xv, yv= self.bezier_curve(self.bezpts,40)  # eval Bezier
-            print("xv=",xv)
-            print("yv=",yv)
-            print("len(xv)=",len(xv))
+            # self.xv, self.yv= self.bezier_curve(self.bezpts,40)  # eval Bezier
+
+            self.bez_plot.append(self.bezpts.copy())
+            print(f"        type(self.bez_plot)={type(self.bez_plot)}")
+            print(f"        len(self.bez_plot)={len(self.bez_plot)}")
+            print(f"        self.bez_plot={self.bez_plot}")
+
+            # print("xv=",xv)
+            # print("yv=",yv)
+            # print("len(xv)=",len(self.xv))
             # self.csv_array = np.empty([1,4])  # should probably *just* np.delete, but meh
             # self.csv_array = np.delete(self.csv_array,0,0)
             # for idx in range(len(xv)):
@@ -205,16 +301,22 @@ class MplWidget(QtWidgets.QWidget):
 
             # self.population_plot[self.discrete_scalar].ax0.plot(xv, yv, label=ctname, linewidth=lw, color=ctcolor)
             # self.ax0.plot(xv, yv, 'o')
-            self.circles(xv,yv, s=rval, c='r', edgecolor='red')
-            self.ax0.plot(xvals, yvals)
-            # self.ax0.set_xlabel('time (mins)')
-            # self.ax0.set_ylabel('# of cells')
-            # self.ax0.set_title("cell_type", fontsize=10)
-            # self.ax0.legend(loc='center right', prop={'size': 8})
-            self.canvas.update()
-            self.canvas.draw()
-            # self.population_plot[self.discrete_scalar].ax0.legend(loc='center right', prop={'size': 8})
-            # self.show()
+
+            # self.circles(self.xv,self.yv, s=rval, c='r', edgecolor='red')
+            # self.xv2, self.yv2= self.cell_spacing(self.xv,self.yv)  # eval Bezier
+
+            # self.ax0.plot(xvals, yvals)   # plot 4-pt control polygon
+            # # self.ax0.set_xlabel('time (mins)')
+            # # self.ax0.set_ylabel('# of cells')
+            # self.ax0.set_title("Press 'c' to clear all.", fontsize=10)
+            # # self.ax0.legend(loc='center right', prop={'size': 8})
+            # self.canvas.update()
+            # self.canvas.draw()
+            # # self.population_plot[self.discrete_scalar].ax0.legend(loc='center right', prop={'size': 8})
+            # # self.show()
+
+            # self.update_plots(xvals,yvals)
+            self.update_plots()
             return
 
         # self.circles(xvals,yvals, s=rvals, color=self.color_by_celltype[cell_type_index], alpha=self.alpha_value)
@@ -225,7 +327,6 @@ class MplWidget(QtWidgets.QWidget):
         self.ax0.set_xlim(self.plot_xmin, self.plot_xmax)
         self.ax0.set_ylim(self.plot_ymin, self.plot_ymax)
 
-        # self.update_plots()
         self.canvas.update()
         self.canvas.draw()
 
